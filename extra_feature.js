f9c13ab716db39b468f2995acffb07954c4f168d
LAZADA\anhhuy.tang <anhhuy.tang@lazada.com>
2018-11-13 11:03:49 +0700

    rename commentbox.js to extra_feature.js; add color highlight feature

---
+++ extra_feature.js
@@ -0,0 +1,71 @@
+// Author: Tang Anh Huy (anhhuy.tang@lazada.com)
+function LoadCommentBox(bridge, id_comment_box, box_height) {
+    id_comment_box = "#" + id_comment_box;
+    var id_comment_tab = "#T" + Math.ceil(Math.random()*1000000);
+    var id_new_box = "#B" + Math.ceil(Math.random()*1000000);
+    var element = $(id_comment_box).detach();
+    bridge.container.innerHTML = "<div id='" + id_comment_tab.substring(1) + "'></div>";
+    $(id_comment_tab).append(element);
+    $(id_comment_box).attr("id",id_new_box.substring(1));
+    $(id_new_box).css("display", "");
+    $(id_new_box).css("width", "100%");
+    $(id_new_box).css("max-height", box_height + "px");
+    $(id_new_box + ' .fui-dialog-foot').remove();
+    $(id_new_box + ' .fui-draggable').remove();
+    $(id_new_box + ' .fui-dialog-close').remove();
+    $(id_new_box).removeClass("fui-dialog");
+    $(id_new_box).css("background-color", "#FFFFFF");
+    $(id_new_box + ' ul:first').css("height", (box_height-90) + "px");
+    $(id_new_box + ' button').wrap("<div id='sbox' style='min-height: 40px'></div>");
+    $(id_new_box + ' #sbox').css("border-bottom", "1px solid #DDDDDD");
+    $(id_new_box + ' button').css("float", "right");
+    $(id_new_box + ' button').css("width", "130px");
+}
+
+function fillColor(colorPositive, colorNegative, reverseToken) {
+	if ($(".pvtTable")[0]) {
+		var isReversed = false;
+		var txt = 'Target';
+		var column = $('table tr th').filter(function() {
+		$(".pvtTable th:contains('Target')").attr("id","targetCol");
+		return $(this).text() === txt;
+		}).index();
+		if(column > -1) {                                           
+			$(".pvtVal.col" + (column-1)).css('background-color', '#EEEEEE');
+			var r = 0;
+			while($(".pvtVal.row" + r + ".col0")[0]) {
+				var target = parseFloat($(".pvtVal.row" + r + ".col" + (column-1)).text());
+				if (isNaN(target)) {
+				r += 1;
+				continue;
+				}
+				for (c = 0; c < column-1; c++) {
+					if (reverseToken != null && reverseToken != "" && c==0){
+						var label = $(".pvtVal.row" + r + ".col" + c).prev().text();
+						if (label.trim().slice(-1) == ".") {
+							isReversed = true;
+						}
+						else {
+							isReversed = false;
+						}
+					}
+
+					var celltext = $(".pvtVal.row" + r + ".col" + c).text();
+					var cellval = parseFloat(celltext);
+
+					$(".pvtVal.row" + r + ".col" + (column-1)).css('background-color', '#EEEEEE');
+					if (isNaN(cellval) || celltext == "") {
+					  continue;
+					}
+					else if (cellval < target) {
+					  $(".pvtVal.row" + r + ".col" + c).css("background-color", $("#btnHighlight").is(":checked") ? (isReversed ? colorPositive : colorNegative) : "#FFFFFF");
+					}
+					else {
+					  $(".pvtVal.row" + r + ".col" + c).css("background-color", $("#btnHighlight").is(":checked") ? (isReversed ? colorNegative : colorPositive) : "#FFFFFF");
+					}
+				}
+				r += 1;
+			}
+		}
+	}
+}
